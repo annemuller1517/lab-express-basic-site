e7532d7372d67ef01c9fd388cb3b308c09011181
Anne Muller <anne_muller@live.nl>
2021-11-09 23:07:07 +0100

    pika sound

--- public/src/homePage.js
+++ public/src/homePage.js
@@ -1,9 +1,13 @@
 const manishPhoto = document.querySelector("#manish")
 
+let pika = new Audio('sounds/pikaaaa.mp3')
+
 manishPhoto.addEventListener("click", () => {
     // manishPhoto.src = 'images/pokemon.png';
     if (manishPhoto.getAttribute("src") == "images/manish.png"){
         manishPhoto.src = 'images/pokemon.png';
+        pika.play()
+
     }
     else {
         manishPhoto.src = "images/manish.png";
